56b49290828c675b4b113c6a8f08def5f77e9407
sajeyyy <sajecowell81@gmail.com>
2024-08-30 16:25:15 -0500

    State Updates
    Smoothed transition between states, and redesigned the states

--- EECS581_Project1.rkt
+++ EECS581_Project1.rkt
@@ -8,20 +8,21 @@
 (define playing 2)
 
 ;; Declare Variables
-(define boardSize 10)  ; Size of the board
-(define cellSize 40)  ; Size of each cell in the grid
-(define x-offset 200)  ; Horizontal offset for grid positioning
-(define y-offset 200)  ; Vertical offset for grid positioning
-(define button-width 160)  ; Width of the start button
-(define button-height 60)  ; Height of the start button
-(define currentState home) ; Start the current state at homescreen
-(define num-ships 0) ; Number of ships selected
+(define boardSize 10)
+(define cellSize 40)
+(define x-offset 200)
+(define y-offset 200)
+(define button-width 160)
+(define button-height 60)
+(define currentState home)
+(define num-ships 0)
+(define char-width 10)
 
 ;; Create the Board
 (define (createBoard size)
-  (make-vector size (make-vector size #f)))  ; False means no part of a ship in that cell
+  (make-vector size (make-vector size #f)))
 
-(define initialBoard (createBoard boardSize))  ; Initialize the board
+(define initialBoard (createBoard boardSize))
 
 ;; Checks if the mouse click is within a given area
 (define (mouse-in? mx my x y width height)
@@ -30,31 +31,31 @@
 
 ;; Game update function
 (define (update state)
-  (let* ((mouseX (mouse-x))  ; Get mouse X position
-         (mouseY (mouse-y))  ; Get mouse Y position
-         (mouseClicked (btn-mouse)))  ; Check if the mouse button is pressed
+  (let* ((mouseX (mouse-x))
+         (mouseY (mouse-y))
+         (mouseClicked (btn-mouse)))
     (cond
-      ;; Transition from home to ship-selection
       [(and (eq? currentState home)
-            (and (>= mouseX x-offset)
-                 (<= mouseX (+ x-offset button-width))
-                 (>= mouseY y-offset)
-                 (<= mouseY (+ y-offset button-height))
-                 mouseClicked))
-       (set! currentState ship-selection)]  ; Transition to ship selection state
+            (mouse-in? mouseX mouseY 340 225 button-width button-height)
+            mouseClicked)
+       (set! currentState ship-selection)
+       (printf "Transitioning to Ship Selection State~n")]
 
-      ;; Handle ship selection
       [(eq? currentState ship-selection)
-       ;; Check if mouse is clicked on any of the options
        (for ([i (in-range 5)])
          (let ((option-y (+ 60 (* i 50))))
-           (when (and (>= mouseX 50)
-                      (<= mouseX 150)
-                      (>= mouseY option-y)
-                      (<= mouseY (+ option-y 40))
+           (when (and (mouse-in? mouseX mouseY 375 option-y 100 40)
                       mouseClicked)
-             (set! currentState playing)  ; Set the state to playing
-             (set! num-ships (+ i 1)))))]))) ; Store the selected number of ships
+             (set! currentState playing)
+             (set! num-ships (+ i 1))
+             (printf "Transitioning to Playing State with ~a ships~n" num-ships))))])))
+
+;; Function to approximate text centering horizontally
+(define (center-text x y width text-str)
+  (let* ((font-width (font-advance))
+         (text-length (* (string-length text-str) font-width))
+         (text-x (+ x (/ (- width text-length) 2))))
+    (text text-x y text-str)))
 
 ;; Function to Draw the State
 (define (draw state)
@@ -63,27 +64,37 @@
     (cond
       ;; Draw Home Menu
       [(eq? currentState home)
-       (text (+ x-offset 20) (+ y-offset 20) "Welcome to Battleship!")
-       ;; Draw button with color
-       (rect x-offset y-offset button-width button-height #:fill #t)  ; Draw filled button
-       (text (+ x-offset 20) (+ y-offset (+ button-height 20)) "Start Game")]
-      
+       (color 7)  ; Set color to white using palette index
+       (rect 300 200 button-width button-height #:fill #t)
+       (color 0)  ; Set color to black using palette index
+       (text 340 225 "Start Game")
+
+       ;; Set the font to a larger size
+       (font wide-font)
+       (color 7)
+       (text 300 100 "Welcome to Battleship!")]
+
       ;; Draw Ship Selection Screen
       [(eq? currentState ship-selection)
+       (color 0)  ; Set color to black for text
+       
+       ;; Set the font to a medium size
+       (font tall-font)
        (text 20 20 "Select the number of ships:")
-       ;; Draw options for selecting the number of ships
        (for ([i (in-range 5)])
          (let ((option-y (+ 60 (* i 50))))
-           (rect 50 option-y 100 40 #:fill #t)  ; Draw a rectangle button for each option
-           (text 60 (+ option-y 30) (format "Option ~a" (+ i 1)))))  ; Display option number
-
-       ;; Draw additional instructions if necessary
+           (color 7)  ; Set color to white for button
+           (rect 350 option-y 100 40 #:fill #t)
+           (color 0)  ; Set color to black for text
+           ;; Correctly use center-text to draw the text
+           (text 375 (+ option-y 20) (format "~a Ships" (+ i 1)))))
        (text 20 300 "Click on a number to select the number of ships.")]
-      
+
       ;; Draw Playing State
       [(eq? currentState playing)
-       (text 20 20 "Game in Progress")
-       ;; Draw the grid
+       (color 7)
+       (font wide-font)
+       (text 340 100 "Place Your Ships!")
        (for ([i (in-range (+ boardSize 1))])
          ;; Vertical lines
          (line (+ x-offset (* i cellSize)) y-offset
@@ -95,8 +106,8 @@
 ;; Game loop function that includes both update and draw
 (define (game-loop)
   (begin
-    (update currentState)  ; Call update with the current state
-    (draw currentState)))  ; Call draw with the current state
+    (update currentState)
+    (draw currentState)))
 
 ;; Start the game loop
 (run game-loop
